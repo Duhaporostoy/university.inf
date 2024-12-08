490beacaf6866c0d5dee2c71aa6e3919b0b28ca7
Duhaporostoy <165923283+Duhaporostoy@users.noreply.github.com>
2024-12-09 00:43:46 +0300

    Add files via upload

---
+++ hw1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cmath>
+#include <algorithm>
+
+int main()
+{
+    float x, y;
+
+    std::cout << "Enter x and y values: ";
+    std::cin >> x >> y;
+
+    double R = pow((x + y), (3 * sin(x)));
+    double S = sqrt(fabs(x)) / log(y);
+
+    double C = std::max(R, S);
+
+    std::cout << "R = " << R << std::endl;
+    std::cout << "S = " << S << std::endl;
+    std::cout << "C = " << C << std::endl;
+
+    system("pause");
+
+    return 0;
+}
