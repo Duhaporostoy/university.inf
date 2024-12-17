cf63a23ceed61c4e7d70d167a7cd63a34de4c98c
Duhaporostoy <165923283+Duhaporostoy@users.noreply.github.com>
2024-12-17 21:59:32 +0300

    Add files via upload

---
+++ Практика 4/pr4.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream> 
+#include <cmath>
+#include <algorithm>
+
+int main()
+{
+    float x, y;
+
+    // Запрос ввода значений x и y
+    std::cout << "Enter x and y values: ";
+    std::cin >> x >> y;
+
+    // Вычисление значения R
+    double R = pow((x + y), (3 * sin(x)));
+
+    // Вычисление значения S
+    double S = sqrt(fabs(x)) / log(y);
+
+    // Вычисление максимального значения между R и S
+    double C = std::max(R, S);
+
+    // Вывод результатов
+    std::cout << "R = " << R << std::endl;
+    std::cout << "S = " << S << std::endl;
+    std::cout << "C = " << C << std::endl;
+
+    // Ожидание нажатия любой клавиши перед завершением программы
+    system("pause");
+
+    return 0; // Завершение программы
+}
